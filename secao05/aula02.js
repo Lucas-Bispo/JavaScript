b585334c4ab880537908143be35894b84bb6684f
Lucas-Bispo <60610011+Lucas-Bispo@users.noreply.github.com>
2020-06-12 08:56:18 -0300

    aprendendo

---
+++ secao05/aula02.js
@@ -0,0 +1,30 @@
+/*
+array contendo 5 elementos 
+[0][1][2][3][4][5]
+
+valores de um aarray
+[50][51][12][53][44][95]
+
+Possui tamanho infinito
+Podemos colcar qualquer tipo de dados
+*/ 
+//forma 1
+var alunos = new Array ('Cristina','Ricardo','Lucas','Maria','Sofia');
+console.log(alunos);
+//forma 2 
+var notas = [1,3,5,7,9];
+console.log(notas);
+
+//criando um vetor vazio
+var dados = [];
+console.log(dados);
+
+//fazendo acesso ao valor de um indice
+console.log(nota[2]);
+
+//Alterando o valor a partir do índice 
+notas[2] = 12;
+console.log(notas);
+
+
+
---
+++ secao05/aula03.js
@@ -0,0 +1,12 @@
+var cursos = [
+    'Programação para leigos',
+    'Algoritimos e lógica de programação',
+    'Programação C',
+    'Programação Java',
+    'Programação Python',
+    'Banco de dados',
+    'Programação web com Django Framework',
+    'Programação em JavaScript',
+];
+console.log(cursos);
+console.log(cursos.length);
